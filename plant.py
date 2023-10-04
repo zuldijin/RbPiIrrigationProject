04da2238730d055f1ee40e107f63c16b6ee31d5b
Ricardo Villa Berger <ricardov.berger@gmail.com>
2023-10-03 22:59:55 -0400

    Changing wait time of absortion to use time instead of a cicle

--- plant.py
+++ plant.py
@@ -5,6 +5,7 @@ import pigpio
 from datetime import datetime
 import time
 import adafruit_mcp3xxx.mcp3008 as MCP
+from enum import Enum
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
 
@@ -27,50 +28,47 @@ class plant():
         self.dryVoltage=2.85
         self.saturatedVoltage=1.3
         self.previousMoisture=0
-        self.status='Reading'
+        self.status=Status()
         
     def calculateMoisture(self):
         return round((((self.saturatedVoltage-self.channel.voltage)/(self.dryVoltage-self.saturatedVoltage))*100)+100,0)
     
         
     def printValues(self):
-        print(self.name+' Status: '+self.status)
+        print(self.name+' Status: '+str(self.status))
         print(self.name+' Moisture: '+ str(self.calculateMoisture())+'%')
+        print(self.name+' Minutes at state: '+str(self.status.getTimeElapsed()))
         
     def now(self):
         return datetime.now()
         
-    def logStatusChange(self):
+    def logMoistureChange(self):
         if(abs(self.calculateMoisture()-self.previousMoisture)>=2):
             file1 = open('/home/zuldijin/Desktop/plant_'+self.name+'.log', 'a+')
             file1.write(str(self.now())+'\tPlant: '+self.name
-            +'\tStatus: '+self.status
+            +'\tStatus: '+str(self.status)
             +'\tADC Voltage: '+ str(self.channel.voltage)
             +'V\tMoisture: '+ str(self.calculateMoisture())+'%\n')
             file1.close()
             self.previousMoisture=self.calculateMoisture()
             
     def irrigate(self):
-        if self.calculateMoisture()<self.wetLevel:
+        if self.status.state==State.Absorbing and self.status.getTimeElapsed()<=5:
+            pass
+        elif self.calculateMoisture()<self.wetLevel:
             self.output.write(self.gpioOutputPort,True)
             print('Irrigating for 4 seconds')  
             irrigationTime=0
+            self.status.changeStatus(State.Irrigating)
             while irrigationTime<4:
-                self.status='Irrigating'
-                self.logStatusChange()
+                self.logMoistureChange()
                 self.printValues()
                 time.sleep(1)
                 irrigationTime=irrigationTime+1
             self.output.write(self.gpioOutputPort,False)
-            absorbWaterTime=0
-            while absorbWaterTime<60:
-                self.status='Absorbing'
-                self.logStatusChange()
-                self.printValues()
-                time.sleep(1)
-                absorbWaterTime=absorbWaterTime+1
+            self.status.changeStatus(State.Absorbing)
         else:
-            self.status='Reading'
+            self.status.changeStatus(State.Reading)
                 
     def wait(self):
             plant.output.write(16,True)
@@ -80,10 +78,26 @@ class plant():
             
     def run(self):
             self.printValues()
-            self.logStatusChange()
-            if self.calculateMoisture()<self.dryLevel or self.status=='Absorbing':
+            self.logMoistureChange()
+            if self.calculateMoisture()<=self.dryLevel or self.status.state==State.Absorbing:
                 self.irrigate()
-    
+                
+class Status():
+    def __init__(self):
+        self.state=State.Reading
+        self.time=datetime.now()
+    def changeStatus(self, state):
+        self.state=state
+        self.time=datetime.now()
+    def getTimeElapsed(self):
+        return int((datetime.now()-self.time).total_seconds() / 60)
+    def __str__(self):
+        return self.state.name
+        
+class State(Enum):
+    Reading = 1
+    Absorbing = 2
+    Irrigating = 3
         
 if __name__ == "__main__":
     plants=[plant("Mint",MCP.P0,18,30,80), 
