79c0911bc16866bf3bb46f773ae1e039c3308404
Ricardo Villa Berger <ricardov.berger@gmail.com>
2023-10-11 16:19:56 -0400

    fixing voltages and logs

--- plant.py
+++ plant.py
@@ -25,33 +25,39 @@ class plant():
         self.output = pigpio.pi()
         self.output.set_mode(gpioOutputPort, pigpio.OUTPUT)
         self.output.write(gpioOutputPort,False)
-        self.dryVoltage=2.85
+        self.dryVoltage=3
         self.saturatedVoltage=1.3
         self.previousMoisture=0
         self.status=Status()
+        self.lastLogTime=datetime.now()
         
     def calculateMoisture(self):
         return round((((self.saturatedVoltage-self.channel.voltage)/(self.dryVoltage-self.saturatedVoltage))*100)+100,0)
     
         
     def printValues(self):
-        print(str(self.status))
         print(self.name+' Moisture: '+ str(self.calculateMoisture())+'%')
-        print(self.name+' Minutes at state: '+str(self.status.getTimeElapsed()))
+        print(self.name+'\t'+str(self.status))
         
     def now(self):
         return datetime.now()
-        
+    
+    def getTimeElapsedInsecods(self):
+        return int((datetime.now()-self.lastLogTime).total_seconds())
+    
     def logMoistureChange(self):
         currentMoisture=self.calculateMoisture()
-        if(abs(currentMoisture-self.previousMoisture)>=2):
+        if((abs(currentMoisture-self.previousMoisture)>=2
+            and self.getTimeElapsedInsecods()>=60)
+            or self.previousMoisture==0):
             file1 = open('/home/zuldijin/Desktop/plant_'+self.name+'.log', 'a+')
             file1.write(str(self.now())+'\tPlant: '+self.name
-            +str(self.status)
+            +'\t'+str(self.status)
             +'\tADC Voltage: '+ str(self.channel.voltage)
             +'V\tMoisture: '+ str(currentMoisture)+'%\n')
             file1.close()
             self.previousMoisture=currentMoisture
+            self.lastLogTime=datetime.now()
             
     def irrigate(self):
         if self.status.state==State.Absorbing and self.status.getTimeElapsed()<5:
@@ -93,7 +99,7 @@ class Status():
     def getTimeElapsed(self):
         return int((datetime.now()-self.time).total_seconds() / 60)
     def __str__(self):
-        return '\tStatus: '+self.state.name+' time at state: '+str(self.getTimeElapsed())
+        return 'Status: '+self.state.name+', for: '+str(self.getTimeElapsed())+' minutes'
         
 class State(Enum):
     Reading = 1
@@ -101,10 +107,10 @@ class State(Enum):
     Irrigating = 3
         
 if __name__ == "__main__":
-    plants=[plant("Mint",MCP.P0,18,30,80), 
+    plants=[plant("Mint",MCP.P0,18,35,80), 
             plant("Bamboo",MCP.P1,23,30,80),
-            plant("sequoia",MCP.P2,24,20,80),
-            plant("Delonix Regia",MCP.P3,25,30,80)]
+            plant("sequoia",MCP.P2,24,50,70),
+            plant("Delonix Regia",MCP.P3,25,50,80)]
     while True:
         for plant in plants:
             plant.run()
